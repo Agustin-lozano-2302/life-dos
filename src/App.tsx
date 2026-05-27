6ac908961034f21c53fc4cdd1f2ec6e8331deb41
Agustin-lozano-2302 <agus.lozano.2302@gmail.com>
2026-05-27 18:25:28 -0300

    Add TasksManagePage and /focus/manage route

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import FocusPage from '@/pages/FocusPage'
 import GoalsPage from '@/pages/GoalsPage'
 import NotesPage from '@/pages/NotesPage'
 import ProjectsPage from '@/pages/ProjectsPage'
+import TasksManagePage from '@/pages/TasksManagePage'
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route element={<AppLayout />}>
         <Route index element={<Navigate to="/focus" replace />} />
         <Route path="focus" element={<FocusPage />} />
+        <Route path="focus/manage" element={<TasksManagePage />} />
         <Route path="projects" element={<ProjectsPage />} />
         <Route path="goals" element={<GoalsPage />} />
         <Route path="notes" element={<NotesPage />} />
